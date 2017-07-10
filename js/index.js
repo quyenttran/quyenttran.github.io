a6aed7138722f685d98160e37da1dc4290f8748a
Quyen Tran <quyeentran@gmail.com>
2017-07-09 22:23:20 -0700

    Add typing animation

---
+++ js/index.js
@@ -0,0 +1,29 @@
+(function() {
+  var app;
+
+  $(document).ready(function() {
+    return app.init();
+  });
+
+  app = {
+    text: "Hello stranger. How are you doing?",
+    index: 0,
+    chars: 0,
+    speed: 100,
+    container: ".text .content",
+    init: function() {
+      this.chars = this.text.length;
+      return this.write();
+    },
+    write: function() {
+      $(this.container).append(this.text[this.index]);
+      if (this.index < this.chars) {
+        this.index++;
+        return window.setTimeout(function() {
+          return app.write();
+        }, this.speed);
+      }
+    }
+  };
+
+}).call(this);
